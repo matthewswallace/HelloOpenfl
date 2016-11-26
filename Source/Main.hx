f5bffa10cd692165f23a0ef76457d3f7d13d2862
Matthew Wallace <matthew.wallace@riotgames.com>
2016-11-26 14:58:42 -0600

    facebook extention with openfl

--- Source/Main.hx
+++ Source/Main.hx
@@ -2,39 +2,90 @@ package;
 
 
 import openfl.Assets;
+import openfl.events.MouseEvent;
 import flash.Lib;
 import ru.stablex.ui.UIBuilder;
 import ru.stablex.ui.widgets.Text;
 import ru.stablex.ui.widgets.VBox;
 import ru.stablex.ui.widgets.Button;
+import ru.stablex.ui.skins.Paint;
+import extension.facebook.LoginHelper;
+import extension.facebook.AccessToken;
 
 
 class Main  {
-	public static function main() {
-		 UIBuilder.init();
-		 var vbox = UIBuilder.create(VBox);
-		 var text = UIBuilder.create(Text, {
-			    text       : 'Hello from OpenFL Again',
-			    embedFonts : true,
-			    format     : {
-			        font   : Assets.getFont('fonts/OpenSans-Regular.ttf').fontName,
-			        italic : true,
-			        color  : 0xFF0000,
-			        size   : 18
-			    }
-			});
-			var button = UIBuilder.create(Button, {
-					text : 'Hello!',
-					embededFonts : true,
-					format : {
-						font : Assets.getFont('fonts/OpenSans-Regular.ttf').fontName
-					}
-			});
+	var mHelper : LoginHelper;
+  var mUserId : String;
+  var loginButton:Button;
+  var errorText:Text;
+  var loggedIn:Bool = false;
 
-			vbox.addChild(text);
-			vbox.addChild(button);
-		 Lib.current.addChild( vbox );
+	public function new() {
+    var screenW = Lib.current.stage.stageWidth;
+    var screenH = Lib.current.stage.stageHeight;
+
+    UIBuilder.setTheme('ru.stablex.ui.themes.ios');
+    UIBuilder.init();
+
+    var vbox:VBox = cast(UIBuilder.create(VBox), VBox);
+    vbox.w = screenW;
+    vbox.h = screenH;
+
+
+    loginButton = cast(UIBuilder.create(Button, {
+      text : 'Login with Facebook',
+      embedFonts:true,
+      format: {
+        font: Assets.getFont('fonts/OpenSans-Regular.ttf').name
+    }
+    }), Button);
+
+    loginButton.addEventListener(MouseEvent.CLICK, onLoginClicked);
+
+
+    vbox.addChild(loginButton);
+
+
+
+    Lib.current.addChild(vbox);
+
+    //fbook stuff
+    mHelper = new LoginHelper(onLoggedIn, onLoggedOut, onLogginError, onLoginCancel);
+    mHelper.init(); // this check if the user was loggedIn before, and trigger onLoggedIn if it was the case.
+
+  }
+
+	public static function main() {
+		 new Main();
 	}
 
+	function onLoginClicked(e : MouseEvent) {
+    if (loggedIn) {
+      mHelper.logOut();
+    } else {
+      mHelper.logIn();
+    }
+  }
+
+
+  function onLoggedIn(){
+    mUserId = AccessToken.getCurrent().getUserId();
+    trace("Your user id is : " + mUserId);
+    loginButton.text = 'Logout';
+    loggedIn = true;
+  }
+
+	function onLoggedOut() {
+    loginButton.text = 'Login with Facebook';
+		loggedIn = false;
+  }
+
+  function onLogginError(e : String) {
+    trace("error : "  + e);
+  }
+
+  function onLoginCancel() {
+    trace("You have to log in to use my awesome app.");
+  }
 
 }
